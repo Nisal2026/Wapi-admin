5ae6999d6b7cd5fa91679784bcc574c31c97c1ad
Nisal2026 <doalogo@gmail.com>
2026-04-18 03:00:08 +0530

    18th

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  env: {
-    NEXT_PUBLIC_API_URL: "https://your-backend-domain.com/api",
-    NEXT_PUBLIC_API_BASE_URL: "/api",
-    NEXT_PUBLIC_STORAGE_URL: "https://your-backend-domain.com/",
-  },
+env: {
+  NEXT_PUBLIC_API_URL: "https://api.chatpanelpro.com/api",
+  NEXT_PUBLIC_API_BASE_URL: "/api",
+  NEXT_PUBLIC_STORAGE_URL: "https://api.chatpanelpro.com",
+},
   redirects: async () => {
     return [
       {
